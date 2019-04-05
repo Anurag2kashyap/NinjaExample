32a0beb8b609c1c8bd67c8062bc4c76f1a08aa6e
vinay <vinay.tripathi.ext@nokia.com>
2019-04-06 01:33:56 +0530

    Adding extent report functionality

--- NinjaApplication/src/main/java/com/qa/ninja/base/TestBase.java
+++ NinjaApplication/src/main/java/com/qa/ninja/base/TestBase.java
@@ -9,11 +9,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import com.aventstack.extentreports.Status;
+import com.qa.ninja.util.TestUtil;
+
 public class TestBase {
 
 	public static Properties prop;
 	public static WebDriver driver;
 	
+	TestUtil ts = new TestUtil();
 	
 	public TestBase()
 	{
@@ -44,8 +48,13 @@ public class TestBase {
 			driver = new FirefoxDriver();
 		}
 		
+		ts.extentHtmlReport();
+		
+		ts.test.log(Status.INFO, "Browser is maximised");
 		driver.manage().window().maximize();
+		ts.test.log(Status.INFO, "Deleted all the cookies");
 		driver.manage().deleteAllCookies();
+		ts.test.log(Status.INFO, "Navigated to the URL");
 		driver.get(prop.getProperty("url"));
 	}
 	
--- NinjaApplication/src/main/java/com/qa/ninja/util/TestUtil.java
+++ NinjaApplication/src/main/java/com/qa/ninja/util/TestUtil.java
@@ -5,9 +5,14 @@ import java.io.FileInputStream;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+
 public class TestUtil {
 	public String userName;
 	public String pwd;
+	public  ExtentTest test;
 
 	public void readExcelFile()
 	{
@@ -19,23 +24,32 @@ public class TestUtil {
 			XSSFSheet sheet = workBook.getSheet("Sheet2");
 			int noOfRows = sheet.getLastRowNum() +1;
 			System.out.println("No of rows in Excel sheet : " + noOfRows);
-			
+
 			for(int i =1; i<noOfRows; i++)
 			{
 				String user = sheet.getRow(i).getCell(0).getStringCellValue();
 				userName = user;
 				System.out.println("User name is : " +  userName);
-			
+
 				String pass = sheet.getRow(i).getCell(1).getStringCellValue();
 				pwd = pass;
 				System.out.println("Password is : " +  pwd);
 			}
-			
+
 		}
-		
+
 		catch(Exception e)
 		{
 			System.out.println(e.getMessage());
 		}
+	}
+
+	public void extentHtmlReport()
+	{
+		ExtentHtmlReporter reporter = new ExtentHtmlReporter("C:\\Users\\ankashya\\git\\NinjaExample\\NinjaApplication\\Report\\ninjaHtmlReport.html");
+		ExtentReports extent = new ExtentReports();
+		extent.attachReporter(reporter);
+
+		test = extent.createTest("MyFirstTest", "This is my first test case sample");
 	}
 }
